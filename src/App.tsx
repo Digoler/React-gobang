0c0a7d8ce0e6a4500881dc676042046e7dc303d4
XIE <2580924475@qq.com>
2023-07-29 17:18:52 +0800

    feat:完善页面基础布局

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,37 @@ import './App.css'
 import { Board } from './pages/Board'
 
 function App() {
-  return <Board></Board>
+  return (
+    <div className="container">
+      <div className="wrap">
+        <div className="black-score">
+          <div className="chess-black">
+            <span>黑</span>
+          </div>
+          <div className="score">积分：999</div>
+        </div>
+        <Board></Board>
+        <div className="white-score">
+          <div className="chess-white">
+            <span>白</span>
+          </div>
+          <div className="score">积分：9</div>
+        </div>
+      </div>
+      <div className="buttons">
+        <button className="btn1">
+          <div className="text">开始</div>
+        </button>
+        <button className="btn2">
+          <div className="text">保存棋盘</div>
+        </button>
+        <button className="btn3">
+          <div className="text">悔棋</div>
+        </button>
+      </div>
+      <div className="float"></div>
+    </div>
+  )
 }
 
 export default App
--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -2,6 +2,22 @@ import React, { useState } from 'react'
 import './index.css'
 import { Cell } from '../Cell'
 
+function judgeWinner() {
+  const numbers = new Array(226)
+    .fill(0)
+    .map((num, index) => index)
+    .slice(1)
+  const lines = []
+  for (let i = 0; i < 15; i++) {
+    let t = []
+    for (let j = i * 15; j < 15 + i * 15; j++) {
+      t.push(numbers[j])
+    }
+    lines.push(t)
+  }
+}
+judgeWinner()
+
 export const Board: React.FC<any> = () => {
   const [round, setRound] = useState(false)
   const [squares, setSquares] = useState(
